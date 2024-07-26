136ec8b22fa02290d1f10ef72e159ba950254a38
Kishan Patel <mail@kishanpatel.dev>
2024-07-26 13:25:17 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mabpri: "#293c47",
+        mabsec: "#3f5765",
+        mabaccent: "#c2ae45"
+      }
+    },  },
+  plugins: [],
+}
+
